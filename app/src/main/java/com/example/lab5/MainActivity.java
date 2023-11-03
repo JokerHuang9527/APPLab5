978a992227fbba4fb58c0c4c9f480288020daea5
JokerHuang9527 <huangpw1031@gmail.com>
2023-11-03 14:22:55 +0800

    master

---
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.lab5;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.lab5.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("請選擇功能");
+                dialog.setMessage("請根據下方按鈕選擇要顯示的物件");
+
+                dialog.setNeutralButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "dialog關閉", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.setNegativeButton("自定義Toast", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        showToast();
+                    }
+                });
+                dialog.setPositiveButton("顯示list", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        showListDialog();
+                    }
+                });
+                dialog.show();
+            }
+        });
+    }
+
+    private void showToast(){
+        Toast toast = new Toast(MainActivity.this);
+        toast.setGravity(Gravity.TOP,0, 50);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        LayoutInflater  inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(R.id.custom_toast_root));
+        toast.setView(layout);
+        toast.show();
+    }
+    private void showListDialog(){
+        final String[] list = {"message1", "message2", "message3", "message4", "message5"};
+        AlertDialog.Builder dialog_list = new AlertDialog.Builder(MainActivity.this);
+        dialog_list.setTitle("使用LIST呈現");
+        dialog_list.setItems(list, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,
+                        "你選的是"+ list[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog_list.show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
